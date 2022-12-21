c0897fd8fa5b7474ecf7f2284bb3fda05738242b
aashish <aashish.kumar@ioanyt.com>
2022-12-21 17:44:21 +0530

    add grantedAuthority

--- src/main/java/com/example/mychatapp/auth/UserRegistration.java
+++ src/main/java/com/example/mychatapp/auth/UserRegistration.java
@@ -6,9 +6,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @Getter
 @Setter
@@ -28,7 +30,7 @@ public class UserRegistration implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.singletonList(new SimpleGrantedAuthority(getUsername()));
     }
 
     @Override
