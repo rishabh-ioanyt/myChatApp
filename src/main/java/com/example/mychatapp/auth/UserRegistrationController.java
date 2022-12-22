7ed46823ee9b96daee88cbb30bc6d1fb96487540
aashish <aashish.kumar@ioanyt.com>
2022-12-22 12:13:17 +0530

    error in css !>>

--- src/main/java/com/example/mychatapp/auth/UserRegistrationController.java
+++ src/main/java/com/example/mychatapp/auth/UserRegistrationController.java
@@ -4,12 +4,10 @@ import com.example.mychatapp.message.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@CrossOrigin
 public class UserRegistrationController {
 
     UserRegistrationService userRegistrationService;
