cb44fea280e2fb9a46b2e6a48c9b2411f013471e
rishabh-ioanyt <rishabh.maurya@ioanyt.com>
2022-12-23 12:14:06 +0530

    error !!

--- src/main/resources/static/chat.js
+++ src/main/resources/static/chat.js
@@ -1,29 +1,35 @@
-let stompClient;
+let stompClient = null;
+let socket;
 
 
 function connect() {
-    let socket= new SockJS("/stomp");
-    stompClient = Stomp.over(socket);
-    let currentUser = document.getElementById("currentUser");
-    connectToChat();
-    stompClient.connect({}, function (frame) {
-        stompClient.subscribe("/topic/messages/" + currentUser.innerText, function (response) {
-            let data = JSON.parse(response.body);
-            $(".output")
-                .append("<span><strong>"
-                    + data.message
-                    + "</strong>: <em> send by "
-                    + data.fromLogin
-                    + "</em></span><br/>");
-            console.log(data.message + " " + currentUser);
+    if (stompClient == null) {
+        socket= new SockJS("/stomp");
+        stompClient = Stomp.over(socket);
+        let currentUser = document.getElementById("currentUser");
+        stompClient.connect({}, function (frame) {
+            stompClient.subscribe("/topic/messages/" + currentUser.innerText, function (response) {
+                let data = JSON.parse(response.body);
+                $(".output")
+                    .append("<span><strong>"
+                        + data.message
+                        + "</strong>: <em> send by "
+                        + data.fromLogin
+                        + "</em></span><br/>");
+                console.log(data.message + " " + currentUser);
+            });
+
+            stompClient.subscribe("/user/topic",function (response) {
+                let data = JSON.parse(response.body);
+                console(data.value);
+            })
         });
-});
+    }
 }
 function disconnected() {
     if (stompClient !== null) {
         stompClient.disconnect();
     }
-    console.log("Disconnected");
 }
 
 function connectToChat( username) {
@@ -40,10 +46,10 @@ function sendMsg() {
     var to = document.getElementById("sendto");
     var text = document.getElementById("msg");
     var currentUser = document.getElementById("currentUser");
-    console.log(to.value);
+    console.log(to.innerText);
     console.log(text.value);
     console.log(currentUser.innerText);
-    stompClient.send("/app/stomp/" + to.value, {}, JSON.stringify({
+    stompClient.send("/app/stomp/" + to.innerText, {}, JSON.stringify({
         fromLogin: currentUser.innerText,
         message: text.value
     }));
